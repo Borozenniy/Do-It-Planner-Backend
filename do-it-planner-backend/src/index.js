87f76d8cb37fb0fb8b0cbf0cf087040129555fa6
Serhii Borozennyi <Serhii@thecareeros.com>
2025-02-07 18:00:07 +0100

    help me

--- do-it-planner-backend/src/index.js
+++ do-it-planner-backend/src/index.js
@@ -1,9 +1,25 @@
-const app = require('./app');
+const express = require('express');
 
-const port = process.env.PORT || 5000;
-//const port = 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+const emojis = require('./emojis');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏',
+  });
 });
+
+router.use('/emojis', emojis);
+
+module.exports = router;
+
+//const app = require('./app');
+
+//const port = process.env.PORT || 5000;
+////const port = 5000;
+//app.listen(port, () => {
+//  /* eslint-disable no-console */
+//  console.log(`Listening: http://localhost:${port}`);
+//  /* eslint-enable no-console */
+//});
