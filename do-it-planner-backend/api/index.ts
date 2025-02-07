f4275044573eca3fd2f19a50345ee90cefa8642e
Serhii Borozennyi <Serhii@thecareeros.com>
2025-02-07 18:17:53 +0100

    ong

--- do-it-planner-backend/api/index.ts
+++ do-it-planner-backend/api/index.ts
@@ -1,8 +1,53 @@
-//const express = require('express');
+const express = require('express');
 const app = express();
+const http = require('http');
+const mongoose = require('mongoose');
+const port = 3000;
+const cors = require('cors');
+require('dotenv').config({ path: '.env' });
 
-app.get('/', (req, res) => res.send('Exprs on Vercel'));
+const server = http.createServer(app);
 
-app.listen(3333, () => console.log('Server ready on port 3333.'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//* CORS
+app.options('*', cors());
+
+//app.use(
+//  cors({
+//    origin: '*',
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//    allowedHeaders: ['Content-Type', 'Authorization'],
+//    credentials: true,
+//  })
+//);
+
+app.use(cors());
+// http://localhost:5173
+
+app.use(express.json()); // Для парсингу JSON тіла запиту
+
+//app.options('*', cors());
+
+//* MongoDB Connection
+
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use('/user', require('./routes/userRoutes'));
+app.use('/goal', require('./routes/goalRoutes'));
+app.use('/subgoal', require('./routes/subgoalRoutes'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 module.exports = app;
