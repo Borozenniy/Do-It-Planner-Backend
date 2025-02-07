9981027f0c9b4eb11f05307c87f6f3d268848e48
Serhii Borozennyi <Serhii@thecareeros.com>
2025-02-07 20:09:44 +0100

    i hope it'll be work

--- do-it-planner-backend/server.js
+++ do-it-planner-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const mongoose = require('mongoose');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config({ path: '.env' });
 
